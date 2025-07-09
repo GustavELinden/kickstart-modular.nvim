40829bf418c259c449d0542a66473394cbcbd124
gustav.linden <gustav.linden@lindendevelop.onmicrosoft.com>
2025-07-09 20:40:16 +0200

    my own nvim setup

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -161,9 +161,7 @@ return {
             end, '[T]oggle Inlay [H]ints')
           end
         end,
-      })
-
-      -- Diagnostic Config
+      }) -- Diagnostic Config
       -- See :help vim.diagnostic.Opts
       vim.diagnostic.config {
         severity_sort = true,
@@ -209,8 +207,10 @@ return {
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
+        gopls = {},
+        powershell_es = {},
+        pyright = {},
+
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -218,9 +218,9 @@ return {
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
-
+        ts_ls = {},
+        tsp_server = {},
+        bicep = {},
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
